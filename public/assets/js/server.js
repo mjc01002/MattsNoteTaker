e82eabc09a3ccd9f0d70698dfa74265fe6345583
stoncalkins@gmail.com <stoncalkins@gmail.com>
2021-12-09 10:31:27 -0500

    updated server.js

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
-app.listen(3002, () => {
-    console.log(`API server now on port 3001!`);
-  });
+const PORT = process.env.PORT || 3002;
 
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+  });
 const { notes } = require('./data/notes');
 
 app.get('/api/notes', (req, res) => {
