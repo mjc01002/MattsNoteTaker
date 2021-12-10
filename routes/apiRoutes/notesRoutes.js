7039fb48f9c9baf32754faf721a5130804774fee
stoncalkins@gmail.com <stoncalkins@gmail.com>
2021-12-10 13:42:59 -0500

    updated routes

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -22,7 +22,7 @@ router.get('api/notes/:id', (req, res) => {
   }
 });
 
-router.post('/notes', (req, res) => {
+router.post('api/notes', (req, res) => {
   // set id based on what the next index of the array will be
   req.body.id = notes.length.toString();
 
